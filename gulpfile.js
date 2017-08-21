52f9386ef688cd9f258ad246f471b2b4a5fc0787
AleksanderGondek <gondekaleksander@hotmail.com>
2017-08-21 22:09:35 +0200

    Added gulp tasks for handling python virtualenv
    * Task for creation of virtualenv and installation of python requirements added

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,22 @@
 "use strict";
 
-var gulp  = require("gulp");
-var gutil = require("gulp-util");
+var exec = require("exec-chainable");
 
-var helpers = require("./gulp-helpers");
+let gulp = require("gulp");
+let gutil = require("gulp-util");
+
+let helpers = require("./gulp-helpers");
+
+gulp.task("createVirtualenv", function() {
+    helpers.log.info("Attempting to create virtualenv..");
+    return exec("virtualenv virtualenv").then(function() {
+        helpers.log.success("virtualenv created!");
+        helpers.log.info("Attempting to install virtualenv requirements..");
+        return exec(require.resolve("./virtualenv/Scripts/pip.exe") + " install -r " + "./requirements.txt").then(function() {
+            helpers.log.info("Requirements installed!!");
+        });
+    });
+});
 
 gulp.task("test", function() {
     return helpers.log.success("Gulp is working!");
