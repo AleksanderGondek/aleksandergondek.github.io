eae577b93c196e34906197b03c77222359de5220
AleksanderGondek <gondekaleksander@hotmail.com>
2017-08-22 21:15:17 +0200

    Small refactoring of gulp files
    * Refactored gulp files to make use of arrow functions and let statements

--- gulp-helpers.js
+++ gulp-helpers.js
@@ -1,17 +1,11 @@
 "use strict";
 
-var gutil = require("gulp-util");
+let gutil = require("gulp-util");
 
-var logger = {
-    success: function(text) {
-        gutil.log(gutil.colors.white.bgGreen(text));
-    },
-    info: function(text) {
-        gutil.log(gutil.colors.white.bgBlue(text));
-    },
-    fail: function(text) {
-        gutil.log(gutil.colors.white.bgRed(text));
-    }
+let logger = {
+    success: (text) => { gutil.log(gutil.colors.white.bgGreen(text)); },
+    info: (text) => { gutil.log(gutil.colors.white.bgBlue(text)); },
+    fail: (text) =>  { gutil.log(gutil.colors.white.bgRed(text)); }
 };
 
 exports.log = logger;
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,26 @@
 "use strict";
 
-var exec = require("exec-chainable");
+let exec = require("exec-chainable");
 
 let gulp = require("gulp");
 let gutil = require("gulp-util");
 
 let helpers = require("./gulp-helpers");
 
-gulp.task("createVirtualenv", function() {
+gulp.task("createVirtualenv", () => {
     helpers.log.info("Attempting to create virtualenv..");
-    return exec("virtualenv virtualenv").then(function() {
-        helpers.log.success("virtualenv created!");
-        helpers.log.info("Attempting to install virtualenv requirements..");
-        return exec(require.resolve("./virtualenv/Scripts/pip.exe") + " install -r " + "./requirements.txt").then(function() {
-            helpers.log.info("Requirements installed!!");
+    return exec("virtualenv virtualenv")
+        .then( () => {
+            helpers.log.success("virtualenv created!");
+
+            helpers.log.info("Attempting to install virtualenv requirements..");
+            return exec(require.resolve("./virtualenv/Scripts/pip.exe") + " install -r " + "./requirements.txt")
+                .then( () => {
+                    helpers.log.info("Requirements installed!!");
+                });
         });
-    });
 });
 
-gulp.task("test", function() {
-    return helpers.log.success("Gulp is working!");
-});
+gulp.task("test", () => { return helpers.log.success("Gulp is working!"); });
 
 gulp.task("default", [ "test" ]);
