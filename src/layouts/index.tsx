683ff24326512847201e10ee628b006ad82c9059
AleksanderGondek <gondekaleksander@hotmail.com>
2018-03-21 10:58:30 +0100

    Refactor files to use 4 spaces indentation and double quotes

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,30 +1,30 @@
-import * as React from 'react'
-import Helmet from 'react-helmet'
+import * as React from "react"
+import Helmet from "react-helmet"
 
-import Header from '../components/Header'
-import './index.css'
+import Header from "../components/Header"
+import "./index.css"
 
-const TemplateWrapper = ({ children }: { children: any} ) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+const TemplateWrapper = ({ children }: { children: any }) => (
+    <div>
+        <Helmet
+            title="Gatsby Default Starter"
+            meta={[
+                { name: "description", content: "Sample" },
+                { name: "keywords", content: "sample, something" },
+            ]}
+        />
+        <Header />
+        <div
+            style={{
+                margin: "0 auto",
+                maxWidth: 960,
+                padding: "0px 1.0875rem 1.45rem",
+                paddingTop: 0,
+            }}
+        >
+            {children()}
+        </div>
     </div>
-  </div>
 )
 
 export default TemplateWrapper
