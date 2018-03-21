6797cbd1220212463e9b4cc7921d5969df8d9ea9
AleksanderGondek <gondekaleksander@hotmail.com>
2018-03-21 15:22:19 +0100

    Add scss processor and remove any styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,29 +2,10 @@ import Link from "gatsby-link";
 import * as React from "react";
 
 const Header = () => (
-    <div
-        style={{
-            background: "rebeccapurple",
-            marginBottom: "1.45rem",
-        }}
-    >
-        <div
-            style={{
-                margin: "0 auto",
-                maxWidth: 960,
-                padding: "1.45rem 1.0875rem",
-            }}
-        >
-            <h1 style={{ margin: 0 }}>
-                <Link
-                    to="/"
-                    style={{
-                        color: "white",
-                        textDecoration: "none",
-                    }}
-                >
-                    Gatsby
-        </Link>
+    <div>
+        <div>
+            <h1>
+                <Link to="/">Gatsby</Link>
             </h1>
         </div>
     </div>
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import Helmet from "react-helmet";
 
 import Header from "../components/Header";
-import "./index.css";
+import "./index.scss";
 
 const TemplateWrapper = ({ children }: { children: any }) => (
     <div>
@@ -14,14 +14,7 @@ const TemplateWrapper = ({ children }: { children: any }) => (
             ]}
         />
         <Header />
-        <div
-            style={{
-                margin: "0 auto",
-                maxWidth: 960,
-                padding: "0px 1.0875rem 1.45rem",
-                paddingTop: 0,
-            }}
-        >
+        <div>
             {children()}
         </div>
     </div>
