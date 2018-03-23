5b26cf559fc558f18098ccae8e28535803360c98
AleksanderGondek <gondekaleksander@hotmail.com>
2018-03-23 16:53:01 +0100

    Add customized html file

---
+++ src/html.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+
+let stylesStr: string;
+if (process.env.NODE_ENV === "production") {
+    try {
+        // tslint:disable-next-line:no-var-requires
+        stylesStr = require("!raw-loader!../public/styles.css");
+    } catch (e) {
+        // tslint:disable-next-line:no-console
+        console.log(e);
+    }
+}
+
+interface IHtmlProps {
+    body: any;
+    bodyAttributes: any;
+    headComponents: any;
+    htmlAttributes: any;
+    preBodyComponents: any;
+    postBodyComponents: any;
+}
+
+module.exports = class HTML extends React.Component<IHtmlProps, {}> {
+    render() {
+        let css: any;
+        if (process.env.NODE_ENV === "production") {
+            css = (
+                <style
+                    id="gatsby-inlined-css"
+                    dangerouslySetInnerHTML={{ __html: stylesStr }}
+                />
+            );
+        }
+        return (
+            <html {...this.props.htmlAttributes}>
+                <head>
+                    <meta charSet="utf-8" />
+                    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+                    <meta
+                        name="viewport"
+                        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                    />
+                    {this.props.headComponents}
+                    {css}
+                </head>
+                <body {...this.props.bodyAttributes}>
+                    {this.props.preBodyComponents}
+                    <div
+                        key={"body"}
+                        id="___gatsby"
+                        dangerouslySetInnerHTML={{ __html: this.props.body }}
+                    />
+                    {this.props.postBodyComponents}
+                </body>
+            </html>
+        );
+    }
+};
