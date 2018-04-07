79b573d45646e16a89f0096dd2b6e3aad8b99fe8
AleksanderGondek <gondekaleksander@hotmail.com>
2018-04-07 17:42:32 +0200

    Introduce basic main page layout
    * Remove not used style
    * Add webiste logo compoment
    * Modify main webiste layout

---
+++ src/components/Logo/logo.tsx
@@ -0,0 +1,20 @@
+import Link from "gatsby-link";
+import * as React from "react";
+
+import "./logo.scss";
+
+// tslint:disable-next-line:no-var-requires
+const logo = require("./logo.png");
+
+class LogoComponent extends React.PureComponent<{}, {}> {
+    render() {
+        return (
+            <div className={"logoComponent"}>
+                <img src={logo} alt="Logo" className={"image"} />
+                <h2>Aleksander's Blog</h2>
+            </div>
+        );
+    }
+}
+
+export { LogoComponent };
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
+import { ClearFix, Col, Container, Row } from "react-grid-system";
 import Helmet from "react-helmet";
 
 import { HeaderComponent } from "../components/Header";
-import "./indexStyle.scss";
+import { LogoComponent } from "../components/Logo/logo";
 
 interface IDefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
     location: { pathname: string };
@@ -11,8 +12,8 @@ interface IDefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
 
 class DefaultLayout extends React.PureComponent<IDefaultLayoutProps, void> {
     render() {
-            return (
-            <div className="mainContainer">
+        return (
+            <Container fluid>
                 <Helmet
                     title="Gatsby Default Starter"
                     meta={[
@@ -20,11 +21,35 @@ class DefaultLayout extends React.PureComponent<IDefaultLayoutProps, void> {
                         { name: "keywords", content: "sample, something" },
                     ]}
                 />
-                <HeaderComponent />
-                <div>
-                    {this.props.children()}
-                </div>
-            </div>
+                <Row debug>
+                    &nbsp;
+                </Row>
+                <Row debug>
+                    <Col xs={2} debug>
+                        {/* It is my understanding that I may not correctly use align
+                          as 'Align.Center' due to bug in TS (https://github.com/Microsoft/TypeScript/issues/16671) */}
+                        <Row debug>
+                            <LogoComponent />
+                        </Row>
+                        <Row debug>
+                            <p>Here will be menu</p>
+                        </Row>
+                    </Col>
+                    <Col xs={10} debug>
+                        <Row debug>
+                            <HeaderComponent />
+                        </Row>
+                        <Row debug>
+                            <div>
+                                {this.props.children()}
+                            </div>
+                        </Row>
+                        <Row debug>
+                            <p>Here will be footer</p>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
