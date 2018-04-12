401cad59ae5d3ee1f300c71f373689fb0535d28a
AleksanderGondek <gondekaleksander@hotmail.com>
2018-04-12 19:18:14 +0200

    Added github and linkedin icons to sidebar

--- src/components/SideBarContent/sideBarContent.tsx
+++ src/components/SideBarContent/sideBarContent.tsx
@@ -1,5 +1,9 @@
 import * as React from "react";
 
+import * as faGithub from "@fortawesome/fontawesome-free-brands/faGithub";
+import * as faLinkedinIn from "@fortawesome/fontawesome-free-brands/faLinkedinIn";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { LogoComponent } from "../../components/Logo/logo";
 import { SideBarNavComponent } from "../../components/SideBarNavigation/sideBarNav";
 
@@ -11,6 +15,10 @@ class SideBarContentComponent extends React.PureComponent<{}, {}> {
             <div className="sideBarContent">
                 <LogoComponent/>
                 <SideBarNavComponent/>
+                <div className="externalPages">
+                    <FontAwesomeIcon icon={faGithub}/>
+                    <FontAwesomeIcon icon={faLinkedinIn}/>
+                </div>
             </div>
         );
     }
