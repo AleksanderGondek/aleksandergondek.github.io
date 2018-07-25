b8dc1fbb98ba22bb70a1223f1a97795b51bb6278
AleksanderGondek <gondekaleksander@hotmail.com>
2018-07-25 12:35:48 +0200

    Add blog posts stats component

---
+++ src/components/BlogPostStats/BlogPostStats.tsx
@@ -0,0 +1,59 @@
+import * as faCalendar from "@fortawesome/fontawesome-free-solid/faCalendar";
+import * as faClock from "@fortawesome/fontawesome-free-solid/faClock";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as React from "react";
+
+import "./BlogPostStatsStyle.scss";
+
+class BlogPostStatsComponent extends React.PureComponent<IBlogPostsStatsProps, {}> {
+    render() {
+        return (
+            <div className="blogPostsStats">
+                <div className="column">
+                    <FontAwesomeIcon icon={faCalendar} />&nbsp;&nbsp;
+                    Posted on {this.props.data.markdownRemark.frontmatter.date}
+                </div>
+                <div className="column">
+                    |
+                </div>
+                <div className="column">
+                    <FontAwesomeIcon icon={faClock} />&nbsp;&nbsp;
+                    {this.props.data.markdownRemark.timeToRead} minutes
+                     ({this.props.data.markdownRemark.wordCount.words} words)
+                </div>
+            </div>
+        );
+    }
+}
+
+interface IBlogPostStats {
+    markdownRemark: {
+        frontmatter: {
+            date: string,
+        }
+        timeToRead: number,
+        wordCount: {
+            words: number,
+        },
+    };
+}
+
+interface IBlogPostsStatsProps {
+    data: IBlogPostStats;
+}
+
+const blogPostsStatsQuery = graphql`
+    fragment blogPostsStatsQuery on RootQueryType {
+        markdownRemark {
+            frontmatter {
+                date(formatString: "DD MMMM YYYY, HH:MM z")
+            }
+            timeToRead
+            wordCount {
+                words
+            }
+        }
+    }
+`;
+
+export { BlogPostStatsComponent, IBlogPostStats, IBlogPostsStatsProps, blogPostsStatsQuery };
