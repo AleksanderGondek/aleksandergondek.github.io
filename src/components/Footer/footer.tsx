ad6175fb64272b2a707a3b92871cda3c6f62e916
AleksanderGondek <gondekaleksander@hotmail.com>
2018-07-26 12:39:29 +0200

    Add ability to preview year-range in copyright in footer

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -6,11 +6,18 @@ import "./FooterStyle.scss";
 
 class FooterComponent extends React.PureComponent<{}, {}> {
     render() {
+        const currentYear = new Date().getFullYear();
+        const copyrightYear = 2018;
+        let copyRightYearText = `${copyrightYear}`;
+        if (currentYear !== copyrightYear) {
+            copyRightYearText = `${copyrightYear} - ${currentYear}`;
+        }
+
         return (
             <div className="footer">
                 {/* TODO: Include copyright automatically */}
                 <p>
-                    &copy; 2018 Aleksander Gondek;&nbsp;
+                    &copy; {copyRightYearText} Aleksander Gondek;&nbsp;
                     Proudly powered by <a href="https://www.gatsbyjs.org">Gatsby.js</a>
                 </p>
             </div>
