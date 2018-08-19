3454e0073c84ee0e19f0bcc03014fc33b07a523c
AleksanderGondek <gondekaleksander@hotmail.com>
2018-08-19 22:54:27 +0200

    Add user to git commit script

--- deployment.sh
+++ deployment.sh
@@ -9,5 +9,7 @@ mv public/* .
 rm -r public
 
 git add -A
+git config user.email "gondekaleksander@hotmail.com"
+git config user.name "Aleksander Gondek"
 git commit -m "Automatic deployment of blog for change ${CIRCLE_SHA1}"
 git push
