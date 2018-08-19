4bb8d32aa0539b8510f14933934af333c3b25c24
AleksanderGondek <gondekaleksander@hotmail.com>
2018-08-19 23:05:49 +0200

    Attempt to configure git before push

--- deployment.sh
+++ deployment.sh
@@ -10,7 +10,5 @@ mv public/* .
 rm -r public
 
 git add -A
-git config user.email "gondekaleksander@hotmail.com"
-git config user.name "Aleksander Gondek"
 git commit -m "Automatic deployment of blog for change ${CIRCLE_SHA1}"
 git push
